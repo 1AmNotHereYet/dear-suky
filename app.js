edcde5398020dc6241c79596fa98b9e8ea4a7779
Bilal Ado <1bilalado@gmail.com>
2024-09-18 23:19:50 +0100

    adjectives displayed after second div appears

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+const adjectives = [
+    'Amazing.',
+    'Beautiful.',
+    'Radiant.',
+    'Lovely.',
+    'Elegant.',
+    'Adorable.',
+    'Magical.',
+    'Serene.',
+    'Gorgeous.', 
+    'Breathtaking.',
+    'Graceful.',
+    'Luminous.',
+    'Gentle.',
+    'Caring.',
+    'Sweet.',
+    'Heartwarming.',
+    'Consistent.',
+    'Kind.',
+    'Soothing.',
+    'Cute.',
+    'Patient.',
+    'Sincere.'
+];
+
+function displayAdjectives() {
+    const adjectiveTag = document.getElementById('adjective');
+    let index = 0;
+
+    function updateAdjective() {
+        if (index < adjectives.length) {
+            adjectiveTag.textContent = adjectives[index];
+            index++;
+
+            setTimeout(updateAdjective, 1000);
+        }
+    }
+
+    updateAdjective();
+}
+
 document.body.addEventListener('click', function() {
 
     const stack1 = document.getElementById('stack-1');
@@ -7,5 +48,6 @@ document.body.addEventListener('click', function() {
 
     setTimeout(function() {
         stack2.style.opacity = 1;
+        displayAdjectives();
     }, 500);
 });
