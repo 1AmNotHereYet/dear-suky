6b2a22b338d375fe52b66fe89bb17c250a10f10a
Bilal Ado <1bilalado@gmail.com>
2024-09-18 23:02:13 +0100

    second div appears after svg fades away

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 document.body.addEventListener('click', function() {
 
     const stack1 = document.getElementById('stack-1');
+    const stack2 = document.getElementById('stack-2');
     
     stack1.style.opacity = 0;
+
+    setTimeout(function() {
+        stack2.style.opacity = 1;
+    }, 500);
 });
